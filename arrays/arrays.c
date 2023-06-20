1896e3c2a07b238652bc8e8c38bb553cabe13be7
Maks328 <96689357+Maksim325@users.noreply.github.com>
2023-06-20 17:17:23 +0300

    added new() and start working on at()

--- arrays/arrays.c
+++ arrays/arrays.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+//void at(int array, int index);
+int* new(int size);
+int at(int *arrptr, int index);
 int main()
 {
-    int array[16] = {};
-    //printf("%d", array[2]);
-    int *arr = array;
-    int index = 2;
-    arr = arr + index;
-    //arr++;
-    int length = sizeof(array) / 4;    
-
-    printf("%d", is_empty(array));
-    //printf("%d", *arr);
-
     return 0;
 }
-int size(int arr[]) // can be better not O(n). In python function len() have a O(1)
+
+//vector realization
+int* new(int size) {
+    int* arr = (int*)malloc(size * sizeof(int));
+    return arr;
+}
+
+int size(int arr[]) // Returned array size. Can be better not O(n). In python function len() have a O(1)
 {
     int size = 0;
 
@@ -28,10 +28,23 @@ int size(int arr[]) // can be better not O(n). In python function len() have a O
     }
     return size;
 }
-int is_empty(int arr[])
+int is_empty(int arr[]) // returned 1 if array is empty
 {
     if (size(arr) == 0){
         return 1;
     }
     return 0;
 }
+int at(int array[], int index) // must return an element by index, resizes if the index is out of range
+{
+    if (index > size(array )) // must resizes if the index is out of range
+    {
+        int* new_array = new(index + 2);
+        array = new_array;
+    }    
+    int *arr = array; 
+    arr = arr + index; //return the element by index using arifmetic of arrays 
+   
+    return *arr;
+}
+
