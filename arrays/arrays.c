584c5ec38bb6d8ebfab0085ff459ad1883421b03
Maks328 <96689357+Maksim325@users.noreply.github.com>
2023-06-21 21:54:05 +0300

    Added a structure for an array

--- arrays/arrays.c
+++ arrays/arrays.c
@@ -1,31 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-//void at(int array, int index);
-int* new(int size);
+typedef struct ImplementationArray {
+    int size;
+    int capacity;
+    int *data;
+} Array;
+
+//temparary
+Array *new(int size);
+int size(Array *arrptr);
+int is_empty(int arr[]);
 int at(int *arrptr, int index);
+void check_addr(void *ptr);
+
 int main()
 {
+    
+    Array *arrptr = new(4);
     return 0;
 }
 
 //vector realization
-int* new(int size) {
-    int* arr = (int*)malloc(size * sizeof(int));
+Array *new(int capacity) {
+    Array *arr = malloc(sizeof(Array));
+    check_addr(arr);
+    arr->size = 0;
+    arr->capacity = capacity;
+    arr->data = (int *)malloc(sizeof(int) * capacity);
+    check_addr(arr->data);
     return arr;
 }
 
-int size(int arr[]) // Returned array size. Can be better not O(n). In python function len() have a O(1)
+int size(Array *arrptr) // Returned array size. Can be better not O(n). In python function len() have a O(1)
 {
     int size = 0;
 
-    int *ptr = arr;
-
-    while (*ptr != '\0') 
+    while (*arrptr->data != '\0') 
     {
-        ptr++;
+        arrptr++;
         size++;
     }
+    arrptr->size = size;
     return size;
 }
 int is_empty(int arr[]) // returned 1 if array is empty
@@ -35,16 +52,24 @@ int is_empty(int arr[]) // returned 1 if array is empty
     }
     return 0;
 }
-int at(int array[], int index) // must return an element by index, resizes if the index is out of range
+int at(Array *arrptr, int index) // must return an element by index, resizes if the index is out of range
 {
-    if (index > size(array )) // must resizes if the index is out of range
+    if (index > *arrptr->data || ) // must resizes if the index is out of range
     {
-        int* new_array = new(index + 2);
-        array = new_array;
-    }    
-    int *arr = array; 
-    arr = arr + index; //return the element by index using arifmetic of arrays 
+        
+        
+    }
+        return *newArray;
+
+       
+    int *arr = *arrptr->data + index; //return the element by index using arifmetic of arrays 
    
     return *arr;
 }
-
+void check_addr(void *ptr)
+{
+    if (ptr = NULL){
+        printf("Unable to allocate memory");
+        exit(EXIT_FAILURE);
+    }
+}
