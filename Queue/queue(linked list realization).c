df9af6c1816e6d9c520a0c43a83505717b6e7fd1
Maks328 <96689357+Maksim325@users.noreply.github.com>
2023-08-30 10:17:02 +0300

    add dequeue()

--- Queue/queue(linked list realization).c
+++ Queue/queue(linked list realization).c
@@ -24,7 +24,7 @@ QPointers *Create_queue(){
 
     return queue;
 }
-
+bool empty(QPointers *q){return q->head == NULL;}
 int destroy_queue(QPointers *queue){
     struct node *temp, *next;
     check_addr(temp);
@@ -35,9 +35,10 @@ int destroy_queue(QPointers *queue){
         free(temp);
         temp = next;
     }
+    free(queue);
 }
 
-int enqueue(QPointers *queue, int value){
+int enqueue(QPointers *q, int value){
     struct node *newnode, *temp;
     newnode = (struct node*)malloc(sizeof(struct node));
     check_addr(newnode);
@@ -46,16 +47,14 @@ int enqueue(QPointers *queue, int value){
     newnode->next = NULL;
 	newnode->data = value;
 
-    if (queue->head == NULL && queue->tail == NULL){
-		queue->head = newnode;
-        queue->tail = newnode;
-		return 1;
-	}
-    else{
-		temp = queue->tail;
-		queue->tail = newnode;
-		newnode->next = temp;
-	}
+    if (q->tail) {
+    q->tail->next = newnode;
+  }
+  q->tail = newnode;
+
+  if (q->head == NULL) {
+    q->head = newnode;
+  }
 }
 
 void check_addr(void *ptr){
@@ -64,15 +63,30 @@ void check_addr(void *ptr){
         exit(EXIT_FAILURE);
     }
 }
-bool empty(QPointers *q){return q->head == NULL && q->tail == NULL;}
+
+int dequeue(QPointers *q){
+    int value = q->head->data;
+    if (empty(q) == true){
+        printf("Dude.. create element first");
+        exit(EXIT_FAILURE);
+    }
+    struct node *temp = q->head;
+
+    
+    q->head = q->head->next;
+    free(temp);
+
+    return value;
+}
+
+
 int main(){
     QPointers *q = Create_queue();
-    /*
-    enqueue(q, 1);
+    //enqueue(q, 1);
     enqueue(q, 2);
     enqueue(q, 3);
     enqueue(q, 4);
-    */
-    printf("%d", empty(q)); // 1 - OK
+    printf("%d\n ", dequeue(q)); // 2 - OK
+
 
 }
