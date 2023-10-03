1aa9a1bc8b85a2bcfce5ea73074d31b6743545b0
Maks328 <96689357+Maksim325@users.noreply.github.com>
2023-10-03 20:24:25 +0300

    add is_powerof2()

--- Bitwise operations/bitwise_operations.c
+++ Bitwise operations/bitwise_operations.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+
+#include <stdbool.h>
 // basic operations
 
 int and(int byte1, int byte2){ 
@@ -19,10 +21,15 @@ int my_Abs(int num){
     return (num ^ bit31) - bit31;
 }
 
+bool is_powerof2(int num){
+    return ((num & (num - 1)) == 0);
+}
+
 int main(){
     printf("4 and 12: %d\n", and(4, 12)); // 4
     printf("4 or 12: %d\n", or(4, 12)); // 12
     printf("4 xor 12: %d\n", xor(4, 12)); // 8
     printf("not 4 : %d\n", not(4)); // -5
     printf("absolute value of -3: %d", my_Abs(-3)); // 3
+    printf("is 128 power of 2: %d", is_powerof2(128));
 }
