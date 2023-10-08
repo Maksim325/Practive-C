49e9c7d9d951bf8d5ee3b979258adb8a9878d19f
Maks328 <96689357+Maksim325@users.noreply.github.com>
2023-10-08 17:06:49 +0300

    add find_hight

--- Binary search trees/binary_search_tree.c
+++ Binary search trees/binary_search_tree.c
@@ -1,6 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define max(x, y) (((x) > (y)) ? (x) : (y))
+#define min(x, y) (((x) < (y)) ? (x) : (y))
+
 typedef struct BstNode{
 	int data;
 	struct BstNode* left;
@@ -64,16 +67,28 @@ int is_in_tree(Node* root, int value){
     }
 }
 
+int find_hight(Node* root){
+	if (root == NULL){
+		return -1;
+	}
+	int left_hight = find_hight(root->left);
+	int right_hight = find_hight(root->right);
+
+	return max(left_hight, right_hight) + 1;
+}
+
 int main(){
     Node *root = NULL; // create an empty BST
 
     root = insert(root, 10);
     root = insert(root, 5);
     root = insert(root, 20);
+    root = insert(root, 25);
+
     print_tree(root);
     //delete_tree(root);
     printf("is 10 in BST: %d\n", is_in_tree(root, 10));
-    
+    printf("hight of the tree: %d\n", find_hight(root));
     printf("the number of nodes in tree: %d",get_node_count(root));
     
 
